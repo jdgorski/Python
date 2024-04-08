1b7f019d310dc45050badd519ab12fe98634280a
Gorski <jdgorski@cps.k12.il.us>
2024-04-08 13:22:59 -0500

    first commit

--- main.py
+++ main.py
@@ -12,18 +12,27 @@
 # # variables are created when you assign a value to it
 # #variables are case sensitive
 # price = 10 #integer variable
-# name = "John" #string variable
-# rating = 4.9 #float variable
-# is_published = True #boolean variable
-# #start all variables with a lower case letter or underscore
-# # use camel case if you want to start with a capital letter
-# # on the next word
-# playerBulls = "michael jordan"
-# #Concatentation to join variables in a sentence
+# # name = "John" #string variable
+# # rating = 4.9 #float variable
+# # is_published = True #boolean variable
+# # #start all variables with a lower case letter or underscore
+# # # use camel case if you want to start with a capital letter
+# # # on the next word
+# # playerBulls = "michael jordan"
+#Concatentation to join variables in a sentence
 # print(name + " is a basketball player")
-# print(name + "has a rating of " + str(rating))
-# #use the str() function to convert a number to a string
-# #the plus (+) sogn is used to concatenate strings
-personName = input("What is your name? ")
-favoriteColor = input("What is your favorite color? ")
-print(personName + )
+# # print(name + "has a rating of " + str(rating))
+# # #use the str() function to convert a number to a string
+# # #the plus (+) sogn is used to concatenate strings
+# personName = input("What is your name? ")
+# favoriteColor = input("What is your favorite color? ")
+# print(personName + )
+
+
+# create variables for the following :
+age = 25
+name = "John"
+song = "Happy Birthday"
+food = "bananas"
+number = 100
+print("")
